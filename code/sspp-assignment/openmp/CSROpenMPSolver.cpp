85e70b8eaa5af3e71b87609d2ab51d145926d663
Mateusz Gąsior <mr.nimelo@gmail.com>
2017-03-14 00:31:16 +0000

    Added simple parallel loops at the outer loops.

--- code/sspp-assignment/openmp/CSROpenMPSolver.cpp
+++ code/sspp-assignment/openmp/CSROpenMPSolver.cpp
@@ -1,18 +1,45 @@
 #include "CSROpenMPSolver.h"
+#include <omp.h>
 
 representations::output::Output tools::solvers::csr::CSROpenMPSolver::solve(representations::csr::CSR & csr, FLOATING_TYPE * b)
 {
-	FLOATING_TYPE *x = new FLOATING_TYPE[csr.N];
+	FLOATING_TYPE *x = new FLOATING_TYPE[csr.M];
 
-	return representations::output::Output(csr.N, x);
+#pragma omp for
+	for (auto i = 0; i < csr.M; i++)
+		x[i] = 0.0;
+
+#pragma omp parallel shared(csr, b, x)
+	{
+		int threads = omp_get_num_threads(),
+			threadId = omp_get_thread_num();
+		int lowerBoundary = csr.M * threadId / threads,
+			upperBoundary = csr.M *(threadId + 1) / threads;
+
+		for (auto i = lowerBoundary; i < upperBoundary; i++)
+		{
+			for (auto j = csr.IRP[i]; j < csr.IRP[i + 1]; j++)
+			{
+				x[i] += csr.AS[j] * b[csr.JA[j]];
+			}
+		}
+	}
+	//TODO: examine omp barriers
+#pragma omp barrier
+
+	return representations::output::Output(csr.M, x);
 }
 
 tools::solvers::csr::CSROpenMPSolver::CSROpenMPSolver(int threads)
 	:threads(threads)
 {
+	omp_set_dynamic(0);
+	omp_set_num_threads(threads);
 }
 
 void tools::solvers::csr::CSROpenMPSolver::setThreads(int threads)
 {
 	this->threads = threads;
+	omp_set_dynamic(0);
+	omp_set_num_threads(threads);
 }
--- code/sspp-assignment/openmp/ELLPACKOpenMPSolver.cpp
+++ code/sspp-assignment/openmp/ELLPACKOpenMPSolver.cpp
@@ -1,18 +1,40 @@
 #include "ELLPACKOpenMPSolver.h"
+#include <omp.h>
 
 representations::output::Output tools::solvers::ellpack::ELLPACKOpenMPSolver::solve(representations::ellpack::ELLPACK & ellpack, FLOATING_TYPE * b)
 {
-	FLOATING_TYPE *x = new FLOATING_TYPE[ellpack.N];
+	FLOATING_TYPE *x = new FLOATING_TYPE[ellpack.M];
+#pragma omp for
+	for (auto i = 0; i < ellpack.M; i++)
+		x[i] = 0.0;
 
-	return representations::output::Output(ellpack.N, x);
+#pragma omp parallel shared(ellpack, b, x)
+	for (auto i = 0; i < ellpack.M; i++)
+	{
+		FLOATING_TYPE tmp = 0;
+		for (auto j = 0; j < ellpack.MAXNZ; j++)
+		{
+			tmp += ellpack.AS[i][j] * b[ellpack.JA[i][j]];
+		}
+
+		x[i] = tmp;
+	}
+
+	//TODO: examine omp barriers
+#pragma omp barrier
+	return representations::output::Output(ellpack.M, x);
 }
 
 tools::solvers::ellpack::ELLPACKOpenMPSolver::ELLPACKOpenMPSolver(int threads)
 	:threads(threads)
 {
+	omp_set_dynamic(0);
+	omp_set_num_threads(threads);
 }
 
 void tools::solvers::ellpack::ELLPACKOpenMPSolver::setThreads(int threads)
 {
 	this->threads = threads;
+	omp_set_dynamic(0);
+	omp_set_num_threads(threads);
 }
