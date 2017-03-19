ef949a724f9d36d0c66e9fda7da831a0be5cf8bd
Mateusz Gąsior <mr.nimelo@gmail.com>
2017-03-19 16:09:34 +0000

    Fixed problems in MatrixMarket header file.

--- code/sspp-assignment/common/MatrixMarket.h
+++ code/sspp-assignment/common/MatrixMarket.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef SSPP_COMMON_MATRIXMARKET_H_
+#define SSPP_COMMON_MATRIXMARKET_H_
 
 #include "MatrixMarketHeader.h"
 #include "MatrixMarketEnums.h"
@@ -15,7 +16,7 @@ namespace sspp {
                                          INDEXING_TYPE & rows, INDEXING_TYPE & columns, INDEXING_TYPE & non_zeros,
                                          std::vector<INDEXING_TYPE> & row_indices,
                                          std::vector<INDEXING_TYPE> & column_indices, std::vector<FLOATING_TYPE> & values);
-      MatrixMarketErrorCodes MatrixMarket::ReadIndices(std::istream &is,
+      MatrixMarketErrorCodes ReadIndices(std::istream &is,
                                                        INDEXING_TYPE & non_zeros,
                                                        std::vector<INDEXING_TYPE>& row_indices,
                                                        std::vector<INDEXING_TYPE>& column_indices,
@@ -27,3 +28,5 @@ namespace sspp {
     };
   }
 }
+
+#endif
--- code/sspp-assignment/transformer/Source.cpp
+++ code/sspp-assignment/transformer/Source.cpp
@@ -15,43 +15,47 @@
 
 int main(int argc, const char** argv) {
   //TODO: Add handling unexpected error.
-  using namespace sspp::io::readers::commandline;
-  std::vector<Argument> arguments =
-  {
-    Argument(ARG_IN_FILE, ArgumentType::Single),
-    Argument(ARG_OUT_FILE, ArgumentType::Single),
-    Argument(FLAG_CSR, ArgumentType::Flag),
-    Argument(FLAG_ELLPACK, ArgumentType::Flag)
-  };
+  try {
+    using namespace sspp::io::readers::commandline;
+    std::vector<Argument> arguments =
+    {
+      Argument(ARG_IN_FILE, ArgumentType::Single),
+      Argument(ARG_OUT_FILE, ArgumentType::Single),
+      Argument(FLAG_CSR, ArgumentType::Flag),
+      Argument(FLAG_ELLPACK, ArgumentType::Flag)
+    };
 
-  sspp::io::readers::commandline::CommandLineParameterReader reader(arguments);
-  reader.Load(argc, argv);
+    sspp::io::readers::commandline::CommandLineParameterReader reader(arguments);
+    reader.Load(argc, argv);
 
-  if(reader.HasArgument(ARG_IN_FILE) && reader.HasArgument(ARG_OUT_FILE)
-     && (reader.HasArgument(FLAG_CSR) || reader.HasArgument(FLAG_ELLPACK))) {
-    sspp::io::readers::MatrixMarketReader mmr;
-    std::string inputFile = reader.GetParameter(ARG_IN_FILE);
-    auto ism = mmr.FromFile(inputFile.c_str());
-    std::fstream fs;
-    std::string outputFile = reader.GetParameter(ARG_OUT_FILE);
+    if(reader.HasArgument(ARG_IN_FILE) && reader.HasArgument(ARG_OUT_FILE)
+       && (reader.HasArgument(FLAG_CSR) || reader.HasArgument(FLAG_ELLPACK))) {
+      sspp::io::readers::MatrixMarketReader mmr;
+      std::string inputFile = reader.GetParameter(ARG_IN_FILE);
+      auto ism = mmr.FromFile(inputFile.c_str());
+      std::fstream fs;
+      std::string outputFile = reader.GetParameter(ARG_OUT_FILE);
 
-    if(reader.HasArgument(FLAG_CSR)) {
-      sspp::tools::transformers::CSRTransformer csrTransformer;
-      auto csr = csrTransformer.Transform(ism);
-      fs.open(outputFile + CSR_EXTENSION, std::fstream::out | std::fstream::trunc);
-      fs << csr;
-      fs.close();
+      if(reader.HasArgument(FLAG_CSR)) {
+        sspp::tools::transformers::CSRTransformer csrTransformer;
+        auto csr = csrTransformer.Transform(ism);
+        fs.open(outputFile + CSR_EXTENSION, std::fstream::out | std::fstream::trunc);
+        fs << csr;
+        fs.close();
+      }
+
+      if(reader.HasArgument(FLAG_ELLPACK)) {
+        sspp::tools::transformers::ELLPACKTransformer ellpackTransformer;
+        auto ellpack = ellpackTransformer.Transform(ism);
+        fs.open(outputFile + ELLPACK_EXTENSION, std::fstream::out | std::fstream::trunc);
+        fs << ellpack;
+        fs.close();
+      }
+    } else {
+      std::cout << "Incorrect command line paramteres!";
     }
-
-    if(reader.HasArgument(FLAG_ELLPACK)) {
-      sspp::tools::transformers::ELLPACKTransformer ellpackTransformer;
-      auto ellpack = ellpackTransformer.Transform(ism);
-      fs.open(outputFile + ELLPACK_EXTENSION, std::fstream::out | std::fstream::trunc);
-      fs << ellpack;
-      fs.close();
-    }
-  } else {
-    std::cout << "Incorrect command line paramteres!";
+  } catch(...) {
+    std::cout << "Unexpected error occured!";
   }
 
   return 0;
