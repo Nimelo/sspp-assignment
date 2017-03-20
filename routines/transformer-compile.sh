55af8d49315164e9450e3a2262b68699110a4103
Mateusz Gąsior <mr.nimelo@gmail.com>
2017-03-20 00:03:51 +0000

    Fixed scripts

--- routines/transformer-compile.sh
+++ routines/transformer-compile.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 echo "source /apps/intel/xe2017/config_intel.sh"
 source /apps/intel/xe2017/config_intel.sh
-echo "icpc -std=c++11 -o transformer code/transformer/*.cpp code/common/*cpp code/common/*h"
-icpc -std=c++11 -o bin/transformer code/transformer/*.cpp code/common/*cpp code/common/*h
+START=$(date +%s.%N)
+echo "icpc -std=c++11 -O3 -o transformer code/transformer/*.cpp code/common/*cpp code/common/*h"
+icpc -std=c++11 -O3 -o bin/transformer code/transformer/*.cpp code/common/*cpp code/common/*h
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+echo "Compiled in:" $DIFF
--- routines/transformer-transformer.sh
+++ routines/transformer-transformer.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-for a in `ls -1 matrices/*.mtx`; do
+for a in `ls -1 -S -r matrices/*.mtx`; do
   echo "bin/transformer -i" $a "-o representations/"$(basename $a) "-csr -ellpack";
+  
+  START=$(date +%s.%N)
   bin/transformer -i $a -o representations/$(basename $a) -csr -ellpack;
+  END=$(date +%s.%N)
+  
+  DIFF=$(echo "$END - $START" | bc)
+  echo "Transformed in:" $DIFF
 done
