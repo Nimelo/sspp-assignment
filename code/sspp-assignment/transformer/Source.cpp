db1703f50f431f3c3cd73bb69a4419c617176961
Mateusz Gąsior <mr.nimelo@gmail.com>
2017-03-31 00:44:01 +0100

    Fixed code formating

--- code/sspp-assignment/transformer/Source.cpp
+++ code/sspp-assignment/transformer/Source.cpp
@@ -1,66 +1,66 @@
-  #include "../common/CommandLineParameterReader.h"
-  
-  #include <iostream>
-  #include <fstream>
-  #include <string>
-  #include "../common/CRS.h"
-  #include "../common/ELLPACK.h"
+#include "../common/CommandLineParameterReader.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "../common/CRS.h"
+#include "../common/ELLPACK.h"
 #include "../common/MarketMatrixReader.h"
 #include "../common/FloatPatternResolver.h"
 #include "../common/CRSTransformer.h"
 #include "../common/ELLPACKTransformer.h"
 
 #define ARG_IN_FILE "-i"
-  #define ARG_OUT_FILE "-o"
-  #define FLAG_CRS "-crs"
-  #define FLAG_ELLPACK "-ellpack"
-  
-  int main(int argc, const char** argv) {
-    //TODO: Add handling unexpected error.
-    try {
-      using namespace sspp::common;
-      std::vector<Argument> arguments =
-      {
-        Argument(ARG_IN_FILE, ArgumentType::Single),
-        Argument(ARG_OUT_FILE, ArgumentType::Single),
-        Argument(FLAG_CRS, ArgumentType::Flag),
-        Argument(FLAG_ELLPACK, ArgumentType::Flag)
-      };
-  
-      CommandLineParameterReader reader(arguments);
-      reader.Load(argc, argv);
-  
-      if(reader.HasArgument(ARG_IN_FILE) && reader.HasArgument(ARG_OUT_FILE)
-         && (reader.HasArgument(FLAG_CRS) || reader.HasArgument(FLAG_ELLPACK))) {
-        std::string inputFile = reader.GetParameter(ARG_IN_FILE);
-        std::fstream fs, in_fs;
-        std::string outputFile = reader.GetParameter(ARG_OUT_FILE);
-  
-        in_fs.open(inputFile, std::fstream::in);
-        FloatPatternResolver resolver;
-        MatrixMarket<float> mm = MatrixMarketReader::Read(in_fs, resolver);
-  
-        if(reader.HasArgument(FLAG_CRS)) {
-          CRS<float> crs = CRSTransformer::transform(mm);
-          fs.open(outputFile + ".crs", std::fstream::out | std::fstream::trunc);
-          fs << crs;
-          fs.close();
-        }
-  
-        if(reader.HasArgument(FLAG_ELLPACK)) {
-          ELLPACK<float> ellpack = ELLPACKTransformer::transform(mm);
-          fs.open(outputFile + ".ellpack", std::fstream::out | std::fstream::trunc);
-          fs << ellpack;
-          fs.close();
-        }
-  
-        in_fs.close();
-      } else {
-        std::cout << "Incorrect command line paramteres!\n";
+#define ARG_OUT_FILE "-o"
+#define FLAG_CRS "-crs"
+#define FLAG_ELLPACK "-ellpack"
+
+int main(int argc, const char** argv) {
+  //TODO: Add handling unexpected error.
+  try {
+    using namespace sspp::common;
+    std::vector<Argument> arguments =
+    {
+      Argument(ARG_IN_FILE, ArgumentType::Single),
+      Argument(ARG_OUT_FILE, ArgumentType::Single),
+      Argument(FLAG_CRS, ArgumentType::Flag),
+      Argument(FLAG_ELLPACK, ArgumentType::Flag)
+    };
+
+    CommandLineParameterReader reader(arguments);
+    reader.Load(argc, argv);
+
+    if(reader.HasArgument(ARG_IN_FILE) && reader.HasArgument(ARG_OUT_FILE)
+       && (reader.HasArgument(FLAG_CRS) || reader.HasArgument(FLAG_ELLPACK))) {
+      std::string inputFile = reader.GetParameter(ARG_IN_FILE);
+      std::fstream fs, in_fs;
+      std::string outputFile = reader.GetParameter(ARG_OUT_FILE);
+
+      in_fs.open(inputFile, std::fstream::in);
+      FloatPatternResolver resolver;
+      MatrixMarket<float> mm = MatrixMarketReader::Read(in_fs, resolver);
+
+      if(reader.HasArgument(FLAG_CRS)) {
+        CRS<float> crs = CRSTransformer::transform(mm);
+        fs.open(outputFile + ".crs", std::fstream::out | std::fstream::trunc);
+        fs << crs;
+        fs.close();
       }
-    } catch(...) {
-      std::cout << "Unexpected error occured!\n";
+
+      if(reader.HasArgument(FLAG_ELLPACK)) {
+        ELLPACK<float> ellpack = ELLPACKTransformer::transform(mm);
+        fs.open(outputFile + ".ellpack", std::fstream::out | std::fstream::trunc);
+        fs << ellpack;
+        fs.close();
+      }
+
+      in_fs.close();
+    } else {
+      std::cout << "Incorrect command line paramteres!\n";
     }
-  
-    return 0;
+  } catch(...) {
+    std::cout << "Unexpected error occured!\n";
   }
+
+  return 0;
+}
